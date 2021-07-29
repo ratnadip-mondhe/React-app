e7d0cc1b910166148002a2000f70b0a52d58847d
Ratnadip Mondhe <rdmondhe96@gmail.com>
2021-07-29 11:30:35 +0530

    imp

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,24 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let id=100;
+  let name="Rohini";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+    <h1> {name} {id} </h1>
+    <h1>Are you with me</h1>
+  </div>);
+ 
+  
 }
 
 export default App;
