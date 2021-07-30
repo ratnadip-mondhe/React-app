d7f40694d1197aa28fc0f31aa1b8eb5f74b36957
Ratnadip Mondhe <rdmondhe96@gmail.com>
2021-07-30 17:51:11 +0530

    gfg

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  let list =["Hello","Hello"];
-  const addNewHello = ()=>{
-    list.push("Hello buddy");
+import {useState} from "react";
+export default function App(){
+  // declaring the variable using react::hooks
+  // array destructuring::react hooks
+  const [list,setList]=useState([]);
+  // arrowe function
+  const addNewhello =() =>{
+    // array clone in
+    const newList=[...list,"Hello World"];
+    setList(newList);
     console.log(list);
   };
-  return (
+  // view
+  // react unware of the changes of local variable
+  return(
     <div>
-      <h1>
-        Working with events and list
-      </h1>
-      <input type="button" value="Say hello" onClick={addNewHello} />
+      <h1>Working with Event and list</h1>
+      <input type="button" value="Say hello" onClick={addNewhello} />
       {list.map((item) =>{
-        return <h3>{item} </h3>;
+        return (
+          <h3>
+            {item} Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi commodi molestias totam similique quam ea excepturi animi. At, quod sequi, dolores culpa optio voluptatem eveniet architecto quae ut enim vero!
+
+          </h3>
+        );
       })}
     </div>
   );
 }
-export default App;
+
+
+//export default App;
