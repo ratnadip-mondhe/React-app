ebabd733becbed169a5ad63bd621c5b9b2542008
Ratnadip Mondhe <rdmondhe96@gmail.com>
2021-07-30 17:20:37 +0530

    zcxvxbzdzfxdfdsfsfddf

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,18 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let title="Good Morning";
-  let list=["Amravati","Akola","Malegaon","Nanded","Goa"];
-  return (
-  <div>
-    <h1>{title}</h1>
-    <ul>
-    {list.map((item)=>{
-      return <li>{item}</li>;
-    })}
-    </ul>
-    </div>
-  );
-}
+  // data member
+  let title="Event demo";
+  // member function
+  let myfunc=()=>{
+    alert("I am click handlerd");
+  };
+  // bining the members inside the jsx{}
 
+return(
+  <div>
+    <h1>{title} </h1>
+    <input type="button" value="Clcick Me" onClick={myfunc} />
+  </div>
+);
+}
 export default App;
